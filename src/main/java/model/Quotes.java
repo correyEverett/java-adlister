ef10b720dba70e3bfac4f469c57664f54b31a5d9
Correy Everett <correy.everett@gmail.com>
2021-01-22 10:20:32 -0600

    create getters and setters for class - Quotes

--- src/main/java/model/Quotes.java
+++ src/main/java/model/Quotes.java
@@ -9,4 +9,23 @@ public class Quotes {
     //    CONSTRUCTOR
     public Quotes() {}
 
+    //    GETTERS AND SETTERS
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
 }
