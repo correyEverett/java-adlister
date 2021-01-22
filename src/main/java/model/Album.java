f1514adc03f04af1ada3e8287cee7295bb3ffe84
Correy Everett <correy.everett@gmail.com>
2021-01-22 10:15:24 -0600

    create fields for class - Album

---
+++ src/main/java/model/Album.java
@@ -0,0 +1,14 @@
+package model;
+import java.util.Date;
+
+public class Album {
+
+//    FIELDS/PROPERTIES
+    private long id;
+    private String artist;
+    private  String name;
+    private Date releaseDate;
+    private long sales;
+    private String genre;
+
+}
