53e206ef411674ae9117f7d52f3f543c03c15012
Correy Everett <correy.everett@gmail.com>
2021-01-22 10:17:58 -0600

    create getters and setters for class - Album

--- src/main/java/model/Album.java
+++ src/main/java/model/Album.java
@@ -14,4 +14,41 @@ public class Album {
     //    CONSTRUCTOR
     public Album() {}
 
+    //    GETTERS AND SETTERS
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public long getSales() {
+        return sales;
+    }
+    public void setSales(long sales) {
+        this.sales = sales;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
